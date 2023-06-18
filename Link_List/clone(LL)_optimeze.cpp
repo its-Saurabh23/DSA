90cc2250946e7f3ae39287fc094efaa2ecc9d7d4
its-Saurabh23 <kavinelevin12345@gmail.com>
2023-06-18 22:33:47 +0530

    new code

--- Link_List/clone(LL)_optimeze.cpp
+++ Link_List/clone(LL)_optimeze.cpp
@@ -1,52 +1,88 @@
-
+/*
 private:
-// insertion at Tail Function. 
-Node* insetAtTail(Node* &head,Node* &Tail,int d){
-   
-   Node* newNode = new Node(d); 
-    if(head == NULL){
+// insertion at Tail Function.
+Node *insetAtTail(Node *&head, Node *&Tail, int d)
+{
+
+    Node *newNode = new Node(d);
+    if (head == NULL)
+    {
         head = newNode;
         tail = newNode;
     }
-    else{
+    else
+    {
         tail->next;
         tail = newNode;
     }
 }
 
 public:
-Node* copyList(Node* head){
-// step 1 create clone Node
-   
-Node* cloneHead = NULL;
-Node* cloneTail = NULL;
-Node* temp = head;
- while( temp !=NULL){
+Node *copyList(Node *head)
+{
+    // step 1 create clone Node
 
-    insertAtTail(cloneHead,cloneTail,temp->data);
-    temp = temp->next;
- }
+    Node *cloneHead = NULL;
+    Node *cloneTail = NULL;
+    Node *temp = head;
+    while (temp != NULL)
+    {
 
- // step 2 add clone lsit in original list
-  
-  Node* originalNode = head;
-  Node* cloneNode = cloneHead;
+        insertAtTail(cloneHead, cloneTail, temp->data);
+        temp = temp->next;
+    }
 
-  while (originalNode =! NULL && cloneHeada !=NULL)
-  {
-    Node* next = originalNode->next;
-    originalNode->next = cloneNode;
-    originalNode = next;
+    // step 2 add clone lsit in original list
 
-    next = cloneNode->next;
-    cloneNode->next = originalNode;
-    cloneNode = next;
-  }
-  
-}
+    Node *originalNode = head;
+    Node *cloneNode = cloneHead;
 
-// step 3 random ponter  
+    while (originalNode = !NULL && cloneHeada != NULL)
+    {
+        Node *next = originalNode->next;
+        originalNode->next = cloneNode;
+        originalNode = next;
 
+        //   for clone node that means next is updated..
+        next = cloneNode->next;
+        cloneNode->next = originalNode;
+        cloneNode = next;
+    }
+    // step 3 random ponter
+    temp = head;
+
+    while (temp != NULL)
+    {
+        if (temp->next != NULL)
+        { 
+            if(temp->random !=NULL){
+ 
+            temp->next->random = temp->random->next;
+            }
+            else{
+                temp ->next = temp ->random;
+            }
+        }
+        temp  =temp->next;
+    }
 // step 4 revert the changes step 2
+   
+   originalNode = head;
+   cloneNode = head;
+
+   while(original !=NULL && cloneNode !=NULL){
+ 
+    originalNode->next = cloneNode->next;
+    originalNode = original->next;
+
+    if(original->next !=NULL){
+      cloneNode->next = originalNode->next;
+    }
+    cloneNode = cloneNode->next; 
+   }
 
 // return ans
+return cloneHead;
+}
+
+*/
