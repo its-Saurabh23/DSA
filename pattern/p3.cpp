8ea9a28978eec1c3c631fdee1577e70f153ff108
its-Saurabh23 <kavinelevin12345@gmail.com>
2023-07-10 21:31:45 +0530

    new change

---
+++ pattern/p3.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n = 3;
+
+
+for(int i =1; i<=n;i++){
+    // rows ke liye
+  
+    for(int j = 1;j<=n;j++){
+    char ch ='A';
+    cout<<" "<<ch;
+    ch = ch+1;
+    }
+       
+    cout<<endl;
+  } 
+/*
+for(int i = 1; i<=n; i++){
+
+    for(int j = 1; j<=n; j++){
+     char ch ='A'+j-1; 
+     cout<<ch;
+    }cout<<endl;
+}
+
+
+
+for(int i = 1; i<=n; i++){
+//   for rows 
+//   int cnt = i;
+   for(int j = 1; j<=i ;j++){
+
+    cout<<" "<<i-j+1;
+    // cout<<" "<<cnt;
+    // cnt =  cnt-1;
+   }cout<<endl;
+}
+*/
+
+    return 0;
+}
