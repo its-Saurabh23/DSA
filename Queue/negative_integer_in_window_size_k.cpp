ff097a12e08de2ce4a7fcfcf383fa0a521ebcb7f
its-Saurabh23 <kavinelevin12345@gmail.com>
2023-07-13 20:13:20 +0530

    new dq wuestion

---
+++ Queue/negative_integer_in_window_size_k.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Given that a function
+*/
+// k = 2;
+int vectro(vector<int>A,int n ,int k){
+
+
+    deque<int>dq;
+    vector<int>ans;
+    // store answer..
+
+    for(int i = 0;i<k;i++){
+        if(A[i]<0){
+         dq.push_back(i);
+        }
+    }
+    // store the negative element in the ans vector
+      if(dq.size()>0){
+      ans.push_back(A[dq.front()]);
+      }
+      else{
+        ans.push_back(0);
+    }
+
+    // rest of the elements.. which is coming to queue
+    for(int i = k; i<n;i++){
+        
+     if(!dq.empty() && i - dq.front()>=k){
+        dq.pop_front();
+     }
+    
+    // addition
+     if(A[i]<0){
+        ans.push_back(i);
+     }
+    //  sotre ans in queue..
+    if(dq.size()>0){
+        ans.push_back(A[dq.front()]);
+    }
+    else{
+        ans.push_back(0);
+    }
+   return ans;
+    }
+}
+
---
+++ Queue/reversl _of_queue.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+int Queue(Queue<int>q){
+
+     stack<int> s;
+
+    // pop from queue
+    while(!q.empty()){
+        int element = q.front();
+        q.pop();
+        s.push(element);
+    }
+
+    while(!s.empty()){
+        int element = s.top();
+        s.pop();
+        q.push(element);
+    }
+    return q;
+}
+*/
+int main()
+{
+    return 0;
+} 
