de9fd2e4ae1d9cd61bfdef60dddff55d70f0cd29
its-Saurabh23 <kavinelevin12345@gmail.com>
2023-07-15 18:04:48 +0530

    new pattern

--- pattern/p2.cpp
+++ pattern/p2.cpp
@@ -4,6 +4,68 @@ using namespace std;
 int main(){
 
 int n = 4;
+
+
+for(int i=1 ;i<=n;i++){
+    // row ke liye
+    int space = n-i+1;
+    for(int j = 1;j<=i-1; j++){
+        cout<<" ";
+    }
+    for(int k = 1;k<space;k++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+
+
+/*
+ for(int i =1; i<=n; i++){
+    // row ke liye 
+     for(int j = 1; j<=n-i+1; j++){
+        cout<<"*";
+     }cout<<endl;
+ }
+
+ 
+for(int i = 4;i>0;i--){
+// row ke liye
+for(int j = 1; j<=i; j++){
+  cout<<"*";
+}cout<<endl;
+}
+
+for(int i =1; i<=n;i++){
+    int sapce = n-i; 
+    for(int j =1; j<=sapce; j++){
+        cout<<" ";
+    }
+    for(int k =1 ; k<i;k++){
+        cout<<"*";
+    }cout<<endl;
+}
+
+
+for(int i = 1;i<=n; i++){
+    // row ke liye
+    char ch ='A'+i-1; 
+    for(int j =1; j<=n; j++){
+        cout<<" "<<ch;
+        ch++;
+    }cout<<endl;
+}
+
+
+for(int i =1; i<=n;i++){
+    // row kw liye 
+    char start = 'A'+n-i;
+    for(int j = 1; j<=i;j++){
+      cout<<" "<<start;
+      start = start+1;
+    }cout<<endl;
+}
+
 for(int i =1; i<=n; i++){
     // For row ke liye
     int cnt = i;
@@ -15,7 +77,7 @@ for(int i =1; i<=n; i++){
 }
 
 
-
+*/
 
 
 /*
--- pattern/tempCodeRunnerFile.cpp
+++ pattern/tempCodeRunnerFile.cpp
@@ -1,6 +1,1 @@
-for(int i =0; i<3-1;i++){
-      cout<< str[i]<<endl;
-       for(int j =0; j<=3; j++){
-        cout<<str[j];
-       }
-   }  
+'A'+n-i
