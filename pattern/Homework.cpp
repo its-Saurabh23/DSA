212d2b70f0f4302ba4fdd0faf75244d6a108c6a8
its-Saurabh23 <kavinelevin12345@gmail.com>
2023-07-16 21:48:41 +0530

    new code

---
+++ pattern/Homework.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n = 4;
+
+    for(int i =1; i<= n; i++){
+        // rows ke liye
+        for(int j =1; i<n; i++){
+            cout<<" ";
+        }
+        for(int k = 1; k<=i; k++){
+            cout<<i;
+        }
+        for(int )
+
+
+
+    }
+ /*
+    for(int i =1; i<=n; i++){
+        // row ke liye 
+
+        for(int j =1; j<=n-i;j++){
+            cout<<" ";
+        }
+            int cnt = i;
+        for(int k = 1; k<=i; k++){
+            cout<<cnt;
+            cnt++;
+        }cout<<endl;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        // row ke liye
+        // int sapce = i-1;
+        for (int j = 1; j <= i - 1; j++)
+        {
+            cout <<" ";
+        }
+        int cnt = i;
+        for (int k = 1; k <= n - i + 1; k++)
+        {
+            cout<<cnt;
+            cnt++;
+        }
+        cout << endl;
+    }
+
+   
+    //  row ke liye
+    for (int i = 0; i <= n; i++)
+    {
+        // int space = n-i;
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int k = 1; k <= i; k++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+
+
+
+        for (int i = 1; i <= n; i++)
+        {
+            // row ke liye
+            for (int j = 1; j <= i - 1; j++)
+            {
+                cout << " ";
+            }
+            for (int k = 1; k <=n-i+1; k++)
+            {
+                cout <<i;
+            }
+            cout << endl;
+        }
+    */
+    return 0;
+}
