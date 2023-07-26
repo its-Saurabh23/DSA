8aff4f9550330b7caa1a86af2a9fce077f625a2e
its-Saurabh23 <kavinelevin12345@gmail.com>
2023-07-26 21:13:27 +0530

    set t=0 for b tree

---
+++ Binary_tree/Boundary_Traversal _binary.cpp
@@ -0,0 +1,76 @@
+/*
+class Solution {
+    private:
+    void TraversalLeft(Node* root,vector<int> &ans){
+        //base case
+        if((root==NULL) ||( root->left == NULL &&root->right == NULL)){
+            return;
+        }
+        ans.push_back(root->data);
+        if(root->left){
+        TraversalLeft(root->left,ans);
+        }
+        else{
+              TraversalLeft(root->right,ans);
+        }
+    }
+    void TraverseLeaf(Node* root , vector<int>&ans){
+        //base case 
+        if(root == NULL){
+            return;
+        }
+        if(root->left == NULL && root->right == NULL)
+          ans.push_back(root->data);
+          
+        TraverseLeaf(root->left,ans);
+        
+        TraverseLeaf(root->right,ans);
+        
+    } 
+    void TraverseRight(Node* root,vector<int>&ans){
+        //base case
+        if((root == NULL) ||(root->left == NULL && root->right == NULL)){
+            return;
+        }
+        if(root->right){
+            TraverseRight(root->right,ans);
+        }
+        else{    
+        TraverseRight(root->left,ans);
+        }
+        
+        //strore ans
+        ans.push_back(root->data);
+    }
+public:
+    vector <int> boundary(Node *root)
+    {
+       
+       vector<int>ans;
+       
+        //base case
+        if(root == NULL){
+            return ans;
+        }
+        ans.push_back(root->data);
+        
+        //traversal for left part
+        TraversalLeft(root->left,ans);
+        
+        // traverse leaf
+        
+        //left subtree
+        TraverseLeaf(root->left,ans);
+        
+        //right subtree
+        TraverseLeaf(root->right,ans);
+        
+        
+        //traverse RightPart
+        
+        TraverseRight(root->right,ans);
+        return ans;
+        
+    }
+};
+*/
---
+++ Binary_tree/zig_zag_traversal.cpp
@@ -0,0 +1,45 @@
+ /*
+ vector <int> zigZagTraversal(Node* root)
+    {
+        vector<int>result;
+        //base case 
+        if(root == NULL){
+            return result;
+        }
+        
+        // creating queue
+        queue<Node*>q;
+        q.push(root);
+        
+        bool leftToRight = true;
+        
+        while(!q.empty()){
+            int size = q.size();
+            
+            vector<int>ans(size);
+            //level Processing 
+            for(int i =0; i<size; i++){
+                Node* frontNode = q.front();
+                q.pop();
+                //Normal inser or reverse insert 
+                int index = leftToRight ? i:size -i-1 ;
+                ans[index] = frontNode->data;
+                
+                if(frontNode->left){
+                    q.push(frontNode->left);
+                }
+                if(frontNode->right){
+                    q.push(frontNode->right);
+                }
+            }
+            //direction change 
+            leftToRight = !leftToRight;
+            
+            for(auto i:ans ){
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+
+    */
