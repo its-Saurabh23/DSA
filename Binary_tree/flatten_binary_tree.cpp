c4251846e6cde2b0bd296e6274677f517cd7d2ea
its-Saurabh23 <kavinelevin12345@gmail.com>
2023-08-19 22:05:00 +0530

    morrries traversal

---
+++ Binary_tree/flatten_binary_tree.cpp
@@ -0,0 +1,28 @@
+/*
+class Solution
+{
+    public:
+    void flatten(Node *root)
+    {
+        
+      Node* curr = root;
+      
+      while(curr != NULL){
+          if(curr->left){
+              
+              Node* pred = curr->left;
+             
+              while(pred->right){
+                    pred = pred->right;
+                    
+              }  
+              pred->right = curr->right;
+              curr->right = curr->left;
+              curr->left = NULL;
+        }
+        curr = curr->right;
+      }
+        
+    }
+
+    */
