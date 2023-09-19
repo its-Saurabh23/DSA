30fe95ec074ef653e6f10bc69aa8c85f8354566b
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-09-19 21:44:45 +0530

    Create two_Sum.cpp
    new question

---
+++ Diff_wepsites_questions/two_Sum.cpp
@@ -0,0 +1,19 @@
+/*
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        for(int i = 0; i<nums.size(); i++){
+            for(int  j = i+1;j<nums.size(); j++){
+                if(nums[i]+nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+          return nums;
+    }
+  
+};
+
+*/
+TC - o(n*n);
