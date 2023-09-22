4d19d61ee2794db0b422ce5c1cecacfb759fab59
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-09-22 14:06:52 +0530

    Majority_Element.cpp
    leetcode

---
+++ Diff_wepsites_questions/Majority_Element.cpp
@@ -0,0 +1,11 @@
+/*
+int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+
+        int n = nums.size();
+          n = n/2;
+        return nums[n];
+    }
+*/
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. Remember this thing
