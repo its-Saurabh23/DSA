c52ec0fc0b7751adb580f1044c3f08d78a6af40d
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-10-01 23:29:39 +0530

    new question

---
+++ Strivers A2Z DSA Course/Superior_Elements.cpp
@@ -0,0 +1,25 @@
+
+
+// Array  element is greater than the maximum  of the right array than
+// it will be the learder/ superior one in the array 
+
+// start iteration form end and cosider lsat element is leader 
+// keep track of maximum element.. 
+/*
+    int n = a.size();
+    vector<int>ans;
+    
+    int maxi = INT_MIN;
+       // o(n)
+    for(int i =n-1; i>=0; i--){
+        if(a[i]>maxi){
+            ans.push_back(a[i]);
+        }
+        maxi = max (maxi,a[i]);
+    }
+
+   // n long n
+    sort(ans.begin(),ans.end());
+    return ans;
+
+    */
