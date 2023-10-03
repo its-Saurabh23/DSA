1e927386212e8de2e2e2b20a9a99447aa19371d3
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-10-03 22:04:23 +0530

    element search in 2 D matrix

---
+++ Diff_wepsites_questions/Search_in_2D_matrix.cpp
@@ -0,0 +1,25 @@
+ //simple travese the 2-d vector and linearch search of that element.. 
+ /* 
+  bool search(vector<vector<int> > matrix, int n, int m, int x) 
+    {
+        
+        if(n==0 && m ==0 ){
+            return -1;
+        }
+        int flage = false;
+        for(int i = 0; i<n; i++){
+            for(int j =0; j<m;j++){
+                if(matrix[i][j]== x){
+                    flage = true;
+                }
+            }
+        }
+        
+        if(flage){
+            return true;
+        }
+        else{
+            return  false;
+        }
+    }
+    */
