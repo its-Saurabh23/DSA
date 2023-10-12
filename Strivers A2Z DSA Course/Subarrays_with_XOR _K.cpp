8418c646165ea26f922d54db10dc57ad7b4788d8
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-10-12 22:55:24 +0530

    new question.cpp

---
+++ Strivers A2Z DSA Course/Subarrays_with_XOR _K.cpp
@@ -0,0 +1,19 @@
+// kadan's algo
+  /*
+  int n = a.size();
+   map<int,int>mpp;
+   int xr = 0;
+   mpp[xr]++;
+
+   int cnt = 0;
+   for(int i =0; i<n;i++)
+   {
+       xr = xr ^ a[i];
+       // for k
+       int x = xr ^ b;
+       cnt  += mpp[x];
+       mpp[xr]++;
+   } 
+   return cnt;
+
+   */
