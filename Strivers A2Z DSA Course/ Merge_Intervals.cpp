9dd00af3d2cbb2606b394e9a1646b10cc4abdf43
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-10-14 22:44:36 +0530

    new question

---
+++ Strivers A2Z DSA Course/ Merge_Intervals.cpp
@@ -0,0 +1,24 @@
+   /*
+   vector<vector<int>> merge(vector<vector<int>>& intervals) {
+       int n = intervals.size();
+       // sort 
+       sort(intervals.begin(),intervals.end());
+
+       vector<vector<int>> ans;
+      for(auto it: intervals){
+          
+          if(ans.empty() || ans.back()[1]<it[0]){
+            
+            // if it not ovarlapping than new one form 
+              ans.push_back(it);
+          }
+          else{
+            // if is in overlapping 
+              ans.back()[1] = max(ans.back()[1],it[1]);
+          }
+      }
+    return ans;
+
+    }
+
+    */
