e3179b1efba6af462eeee5cf08a1adfdd33ad9b9
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-10-20 23:44:41 +0530

    Update Missing_And_Repeating_Numbers.cpp
    optimal solution

--- Strivers A2Z DSA Course/Missing_And_Repeating_Numbers.cpp
+++ Strivers A2Z DSA Course/Missing_And_Repeating_Numbers.cpp
@@ -21,3 +21,31 @@ vector<int> findMissingRepeatingNumbers(vector < int > a) {
   return {repeted,missing};
   
 }
+
+// opitimal solution will be
+vector<int> findMissingRepeatingNumbers(vector < int > a) {
+     long long n = a.size();
+     long long sn =(n*(n+1))/2; // sum of n sum 
+     long long s2n = (n*(n+1) *(2*n+1))/6;  // sum square of natural number
+
+     // array sum 
+     
+     long long s = 0,s2 = 0;
+     for(int i = 0; i<n; i++){
+         // addtion
+        s +=a[i];   
+        //square
+        s2 += (long long) a[i] *(long long)a[i];     
+     }
+  
+  // creating quation
+   long long val1 = s-sn;  // x-y;
+   long long val2 = s2-s2n;  //X2-Y2;
+   val2 = val2 /val1;
+   long long x = (val1 +val2)/2; //x+y
+   long long y = x - val1; 
+
+   return {(int)x, (int)y};
+     
+       
+}
