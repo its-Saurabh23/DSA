ddb7956670e803f6292b68cc90f001854f91cfe1
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-10-22 21:42:39 +0530

    new question

---
+++ Strivers A2Z DSA Course/Tree_series/Pree_order_itrative.cpp
@@ -0,0 +1,25 @@
+  /*
+  // an vector
+    vector<int>ans;
+     if(root == NULL) return  ans;
+    //stack 
+     stack<TreeNode*>st;
+
+    // insedrting first elemtn;
+     st.push(root);
+     while(!st.empty()){
+        //  top of the stack i.e first element
+      root = st.top();
+      // removel of thhe forst element 
+      st.pop();
+      ans.push_back(root->data);
+
+      if(root->right !=NULL){
+        st.push(root->right);
+      }
+      if(root->left !=NULL){
+      st.push(root->left);
+      }
+     }
+     return ans;
+     */
