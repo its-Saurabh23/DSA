51f2f2bf8bbc0d0887cb802e262bb52dbbabde3b
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-11-02 21:06:05 +0530

    new change

--- Binary_tree/BST_to_greater_sum_tree.cpp
+++ Binary_tree/BST_to_greater_sum_tree.cpp
@@ -1,29 +1,30 @@
+//logic apply Reverse InorderTraversal order 
 /*
 class Solution
 {
     public:
     
-    void reversePreorder(struct Node* root,int &sum){
+    void InorderTraversal(struct Node* root,int &sum){
         // base case
         if(root == NULL){
             return;
         }
         // Reverse inorder
         
-        reversePreorder(root->right,sum);
+        InorderTraversal(root->right,sum);
         
         
         int currentSum = root->data;
         root->data =  sum;
         sum = sum + currentSum;
         
-        reversePreorder(root->left,sum);
+        InorderTraversal(root->left,sum);
         
     }
     void transformTree(struct Node *root)
     {
       int sum = 0;
-      reversePreorder(root,sum);
+      InorderTraversal(root,sum);
     }
 
     */
