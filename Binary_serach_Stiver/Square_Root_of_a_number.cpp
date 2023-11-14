bf2c8dea15263b6a99e8aed73b1d253f134a5900
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-11-14 13:31:56 +0530

    square root of a number

---
+++ Binary_serach_Stiver/Square_Root_of_a_number.cpp
@@ -0,0 +1,18 @@
+int floorSqrt(int n)
+{
+int low = 1, high =n;
+ 
+while(low<=high){
+    long long mid = (low+high)/2;
+    long long val = (mid*mid);
+
+    if(val<=n){
+        low = mid +1;
+    }
+    else{
+        high = mid-1;
+    }
+}
+return high;
+
+}
