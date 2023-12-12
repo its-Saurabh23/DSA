88abe29acb2a7ff7c92f6f7280a611b30e82e44d
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2023-12-12 21:15:57 +0530

    new question

--- Diff_wepsites_questions/Element_Appearing_More_Than_25%_In_Sorted_Array.cpp
+++ Diff_wepsites_questions/Element_Appearing_More_Than_25%_In_Sorted_Array.cpp
@@ -1,3 +1,24 @@
+
+// T.C = O(n*n) because of two loops
+// S.C O(1)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                ans = max(ans, (nums[i] - 1) * (nums[j] - 1));
+            }
+        } 
+        return ans;  
+    }
+};
+
+
+// T.C = o( n long n)
+// because of sorting 
+// S.C O(1)  if we are not calculating the sorting space 
+ 
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
@@ -9,5 +30,28 @@ public:
         }
     } 
     return -1;
+    }
+};
+
+// T.C O(n) because of single itration.
+// S.C O(1)
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int largest  = 0;
+        int secondLargest = 0;
+
+        for(int i: nums){
+            if(i>largest){
+                
+               secondLargest = largest ;
+                largest = i;
+            }
+            else{
+              secondLargest = max(secondLargest , i);
+            }
+        }
+        return (largest-1)*(secondLargest-1);
     }
 };
