f13d3ba426bbde7c2e4a778c4aacc2644488e1ff
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-01-03 23:57:09 +0530

    graph DFS

---
+++ Graph/DFS.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+
+   void dfs(int node,vector<int>adj[],vector<int>&visited,vector<int>&ans){
+     
+
+       ans.push_back(node);
+        visited[node]= 1;       
+       
+       for(auto it:adj[node]){
+           if(!visited[it]){
+               dfs(it,adj,visited,ans);
+           }
+       }
+       
+   }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        
+        vector<int>visited(V,0);
+        vector<int>ans;
+        
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+            dfs(i,adj,visited,ans);
+        }
+    }
+    return ans;
+    }
+};
