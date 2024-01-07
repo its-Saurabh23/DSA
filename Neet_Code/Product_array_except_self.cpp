2cc5d25e143d82bdf1bcaac17bad0e8bb9eaa119
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-01-07 22:53:48 +0530

    new question

---
+++ Neet_Code/Product_array_except_self.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {       
+        int n = nums.size();
+        int prefix = 1;
+        vector<int>ans(n,1);
+
+// left to right Traversal  
+        for(int i =0; i<n;i++){
+        ans[i] = prefix;
+        prefix = prefix*nums[i];
+        }
+
+// right to left taversal 
+       int postfix = 1;
+        for(int i = n-1;i>= 0; i--){
+         ans[i] = ans[i]* postfix;
+         postfix = postfix * nums[i];
+        }
+        return ans;
+    }
+};
