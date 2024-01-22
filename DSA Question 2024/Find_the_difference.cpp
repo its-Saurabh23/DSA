2255431bf23fdcf89cb5e13b0b53d00acb29f281
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-01-22 15:31:06 +0530

    new

---
+++ DSA Question 2024/Find_the_difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>mp;
+
+        for(auto &it :s){
+            mp[it]++;
+        }
+
+        for(auto &it :t){
+            
+            mp[it]--;
+            
+            if(mp[it]<0){
+                return it;
+            }
+        }
+        return 'x';
+    }
+};
