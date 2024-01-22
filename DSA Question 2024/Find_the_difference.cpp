6ae1a93b1d0d0a5e5cc2224e835623798412d1e1
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-01-22 15:43:55 +0530

    Anotehr Apporach

--- DSA Question 2024/Find_the_difference.cpp
+++ DSA Question 2024/Find_the_difference.cpp
@@ -18,3 +18,47 @@ public:
         return 'x';
     }
 };
+
+
+
+// another apporach
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int T_sum = 0;
+
+        for(auto &it :t){
+            T_sum += it;
+        }
+
+        int S_sum = 0;
+        for(auto &it: s){
+            S_sum += it;
+        }
+        // return the Difference char form
+
+        return (char) (T_sum - S_sum);
+
+    }
+};
+
+
+//Another Apporach
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int XOR  = 0;
+
+        for(auto it: s){
+            XOR ^= it;
+        }
+
+        for(auto it:t){
+            XOR ^= it;
+        }
+
+        return (char) XOR;
+    } 
+};
