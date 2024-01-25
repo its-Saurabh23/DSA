e1966b6b72087cd86d39a654f7a1037c60355e59
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-01-25 22:37:59 +0530

    comment

--- DSA Question 2024/Find_the_index_of_first_occurance.java
+++ DSA Question 2024/Find_the_index_of_first_occurance.java
@@ -4,3 +4,8 @@ class Solution {
 
     }
 }
+
+// indexOf method to find the index of the first occurrence 
+// of the needle substring within the haystack string.
+// The indexOf method returns the index of the first occurrence of the specified substring,
+//  or -1 if the substring is not found.
