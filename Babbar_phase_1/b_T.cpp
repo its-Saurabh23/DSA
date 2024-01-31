216947f0149bc89942bf8cde445c0ad3c72092fb
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-01-31 21:36:33 +0530

    updeate Binary to decimal

--- Babbar_phase_1/b_T.cpp
+++ Babbar_phase_1/b_T.cpp
@@ -38,3 +38,28 @@ cout<<"Answer "<<ans<<endl;
 
     return 0;
 }
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num ;
+    std::cin >> num;
+    int ans = 0; int rem; int mul = 1;
+    while(num>0){
+            //   getting the single value 
+        rem = num%10;
+            // reducing the value 
+        num = num/10;
+                        // adding previous ans 
+        ans = mul * rem +  ans;
+        
+        mul = mul *2;
+    }
+std::cout << ans << std::endl;
+    return 0;
+}
