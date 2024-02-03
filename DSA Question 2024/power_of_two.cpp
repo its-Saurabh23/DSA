7f104080ead34da1a3b30979e5a120ccf48ea6d8
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-02-04 00:36:24 +0530

    new question

---
+++ DSA Question 2024/power_of_two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 0 || n <0){
+            return false;
+        }
+
+        while(n != 1){
+            if(n %2 == 1){
+                return false;
+            }
+            n /= 2;
+        }
+        return true;
+    }
+};
