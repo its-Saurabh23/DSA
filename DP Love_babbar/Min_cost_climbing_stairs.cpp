840d56873b3ef6a1cf6a2dc6c29972383eb4fe7c
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-02-12 14:35:47 +0530

    optimization

--- DP Love_babbar/Min_cost_climbing_stairs.cpp
+++ DP Love_babbar/Min_cost_climbing_stairs.cpp
@@ -23,6 +23,8 @@ public:
 };
 
 // DP + Memoaization  This include exta space.
+// T.C = O(n)
+//S.C = O(n) +O(n)
 class Solution {
 public:
     int solve(vector<int>&cost,int n,vector<int>&dp){
@@ -47,5 +49,60 @@ public:
         vector<int>dp(n+1,-1);
         int ans = min(solve(cost,n-1,dp), solve(cost,n-2,dp) );
         return ans;
+    }
+};
+
+
+// Tabulation  
+//T.C = O(n);
+// S.C = O(n);
+class Solution {
+public:
+   // Tabulation method
+    int solve(vector<int>&cost,int n){
+   
+   // create 1 dp array
+   vector<int>dp(n+1);
+
+   // base case analysis
+   dp[0] = cost[0];
+   dp[1] = cost[1];
+
+   // step 3 rest of the items solution
+   for(int i = 2;i<n; i++){
+       dp[i] = cost[i]+ min(dp[i-1],dp[i-2]);
+   }
+    return min(dp[n-1],dp[n-2]);
+
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+
+        return solve(cost,n);
+    }
+};
+
+
+//T.C = O(n)
+//S.C = O(1);
+
+class Solution {
+public:
+
+   int solve(vector<int>&cost,int n){
+     int prev2 = cost[0];
+     int prev1 = cost[1];
+
+     for(int i =2; i<n; i++){
+         int curr = cost[i] + min(prev1,prev2);
+         prev2 = prev1;
+         prev1 = curr;
+      }
+      return min(prev1,prev2);
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+         int n = cost.size(); 
+        return solve(cost,n);
     }
 };
