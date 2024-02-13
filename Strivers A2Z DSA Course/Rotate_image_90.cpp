d2564dbdf3ee6d4d17768a050e2d554e3064d214
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-02-13 14:01:01 +0530

    Update Rotate_image_90.cpp
    rotation of image in anticlockwise

--- Strivers A2Z DSA Course/Rotate_image_90.cpp
+++ Strivers A2Z DSA Course/Rotate_image_90.cpp
@@ -8,12 +8,28 @@
    }
  }
 
- // Now to the reversal of the mat
+ // Now to the reversal of the mat in row wise
    
    for(int i = 0; i<n; i++ ){
      reverse(mat[i].begin(),mat[i].end());
    }
 
-
+//This is rotation in clockwies direction 
  // Brute for
  // use another martize and copy the original in an appropate manner..  
+
+
+
+//This is rotation in  anti clockwies direction 
+	void rotateMatrix(vector<vector<int>>& arr, int n) {
+    //  step 1  do the Transpose of array 
+    for (int i = 0; i < n; i++){
+           for (int j = i; j < n; j++){
+                  swap(arr[i][j], arr[j][i]);
+	        }
+	   }
+	        // reverse Columns
+        for (int i = 0; i <n/2; i++){
+               swap(arr[i],arr[n-i-1]);
+           }
+ 	}
