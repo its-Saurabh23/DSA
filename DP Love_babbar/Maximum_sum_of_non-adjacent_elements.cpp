08bcc8d2e9ac4fc5a4e2e86ff835f823ba7373dd
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-02-18 23:40:24 +0530

    space optimsatioln

--- DP Love_babbar/Maximum_sum_of_non-adjacent_elements.cpp
+++ DP Love_babbar/Maximum_sum_of_non-adjacent_elements.cpp
@@ -21,3 +21,87 @@ int maximumNonAdjacentSum(vector<int> &nums){
     int ans = solve(nums,n-1);
      return ans;
 }
+
+// With memoisatoin
+
+// Also giving TLE 
+
+int solve2(vector<int>&nums,int n,vector<int>&dp){
+    // base case
+       if(n<0){
+           return 0;
+       }
+       if(n == 0){
+           return nums[0];
+       }
+     if(dp[n] != -1){
+         return dp[n];
+     }
+        
+    //    recursive call;
+    int incl = solve(nums,n-2) + nums[n]; 
+    int excl = solve(nums,n-1) + 0;
+    
+    dp[n] = max(incl,excl);
+
+    return dp[n]; 
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+     
+    // int n = nums.size();
+    // int ans = solve(nums,n-1);
+    //  return ans;
+
+    int n = nums.size();
+    // dp array
+    vector<int>dp(n+1,-1);
+    return solve2(nums,n-1,dp);
+}
+
+
+
+
+// Tabulation method.
+// with some space 
+
+int solve2(vector<int>&nums,int n,vector<int>&dp){
+    // base case
+       if(n<0){
+           return 0;
+       }
+       if(n == 0){
+           return nums[0];
+       }
+     if(dp[n] != -1){
+         return dp[n];
+     }
+        
+    //    recursive call;
+    int incl = solve(nums,n-2) + nums[n]; 
+    int excl = solve(nums,n-1) + 0;
+    
+    dp[n] = max(incl,excl);
+
+    return dp[n]; 
+}
+
+
+// space optimasation  O(1) ,O(n);
+int solve4(vector<int>&nums){
+    // with space optimisation
+    
+    int n = nums.size();
+    int pre2 = 0;
+    int pre1 = nums[0];
+    
+    for(int i = 1; i<n; i++){
+        int incl = pre2 + nums[i];
+        int excl = pre1 + 0;
+        int ans = max(incl,excl);
+
+        pre2 = pre1;
+        pre1 = ans;
+    }
+
+    return pre1;
+}
