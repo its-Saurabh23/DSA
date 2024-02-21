565b44e870379a75d7778662a22e5176ad9d03c6
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-02-21 22:22:17 +0530

    tabulation

--- DP Love_babbar/Cut_segments_rod_cutting.cpp
+++ DP Love_babbar/Cut_segments_rod_cutting.cpp
@@ -51,3 +51,32 @@ int c = solve(n-z, x,y,z,dp) +1;
 // return the dp ans; 
     return dp[n];
 }
+
+// Tabulation 
+
+int solve(int n,int x,int y,int z){
+
+  vector<int>dp(n+1,INT_MIN);
+  dp[0] = 0;
+
+  for(int i = 1; i<=n; i++){
+   if(i-x>=0){
+     dp[i] = max(dp[i],dp[i-x] + 1);
+   }
+
+   if(i-y>=0){
+     dp[i] = max(dp[i],dp[i-y] + 1);
+   }
+
+   if(i-z>=0){
+     dp[i] = max(dp[i],dp[i-z] + 1);
+   }
+}
+  if(dp[n]<0){
+	  return 0;
+  }
+  else{
+    return dp[n];
+   }
+
+}
