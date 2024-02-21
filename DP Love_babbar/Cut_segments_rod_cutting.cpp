f7352f51eb25fe4914c3f68c3ba0e626e146da39
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-02-21 22:09:16 +0530

    dp otimization

--- DP Love_babbar/Cut_segments_rod_cutting.cpp
+++ DP Love_babbar/Cut_segments_rod_cutting.cpp
@@ -28,3 +28,26 @@ int cutSegments(int n, int x, int y, int z) {
         return ans;
 	 }
 }
+
+// With Memoiazation
+int solve(int n,int x,int y,int z ,vector<int>& dp){
+  
+//   base case
+
+if(n ==0)return 0;
+
+if(n<0)return INT_MIN;
+
+// step 3 check before the recusive call
+if(dp[n] != -1) return dp[n];
+
+int a = solve(n-x, x,y,z,dp) +1;
+int b = solve(n-y, x,y,z,dp) +1;
+int c = solve(n-z, x,y,z,dp) +1;
+
+// step 1 sotore in dp
+   dp[n] = max(a,max(b,c));
+
+// return the dp ans; 
+    return dp[n];
+}
