5f343390d947f6f174fc59f67db7cd2940104e21
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-02-24 15:49:25 +0530

    Update Square_Root_of_a_number.cpp
    overflow condition update

--- Binary_serach_striver/Square_Root_of_a_number.cpp
+++ Binary_serach_striver/Square_Root_of_a_number.cpp
@@ -16,3 +16,32 @@ while(low<=high){
 return high;
 
 }
+
+// To overcome the overflowconditon
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        if(x<2){
+            return x;
+        }
+        int start = 1,end = x,ans,mid;
+
+        while(start<=end){
+            mid = start + (end-start)/2;
+
+            if(mid == x/mid){
+                return mid;
+            }
+            else if(mid<x/mid){
+                // may be a potential ans
+                ans = mid; 
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
