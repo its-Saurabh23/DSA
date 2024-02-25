3a15eae5b913b48c7bac36f39683d84980a376f6
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-02-25 22:13:26 +0530

    otimization

--- DSA Question 2024/Kth_Missing_positive_numer.cpp
+++ DSA Question 2024/Kth_Missing_positive_numer.cpp
@@ -20,3 +20,29 @@ public:
         return nums-1;
     }
 };
+
+//  T.C = log n
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        int l = 0,r = n-1;
+        int mid = -1;
+
+        while(l<=r){
+            mid = l+(r-l)/2;
+
+        int missing  = arr[mid] - (mid + 1);
+        
+        if( missing < k){
+            l = mid + 1;
+        }
+        else{
+            r = mid - 1;
+        }
+    }
+        return l + k;
+
+    }
+}; 
