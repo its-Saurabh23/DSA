e52b496ce94e4137baff32cbea9ea2158329328f
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-02-27 21:46:17 +0530

    Update Search_in_Rotated_Sorted_Array.cpp

--- Binary_serach_striver/Search_in_Rotated_Sorted_Array.cpp
+++ Binary_serach_striver/Search_in_Rotated_Sorted_Array.cpp
@@ -27,4 +27,44 @@
             }
         }
         return -1;
+    
     }
+
+
+
+
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+      int n = nums.size();
+      int start = 0,end = n-1, mid;
+
+      while(start<=end){
+          mid  = start + (end - start)/2;
+           
+          if(nums[mid] == target)
+          return mid;
+        //   left sorted      
+          else if(nums[mid]>= nums[0]){
+              if(nums[start]<= target && nums[mid]>= target){
+                  end = mid - 1;
+              }
+              else{
+                  start = mid + 1;
+              }
+          }
+        //   Right sorted
+          else{
+              if(nums[mid]<= target && nums[end] >= target){
+                  start = mid + 1;
+              }
+              else{
+                 end = mid -1; 
+              }
+          }
+        }
+        return -1;
+    }
+};
