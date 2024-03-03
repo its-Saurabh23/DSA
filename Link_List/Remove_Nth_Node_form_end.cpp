550858fb49c69e8766130f5f4834f60bb4aca60d
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-03-03 22:26:31 +0530

    one pass solution

--- Link_List/Remove_Nth_Node_form_end.cpp
+++ Link_List/Remove_Nth_Node_form_end.cpp
@@ -36,3 +36,44 @@ public:
         return head; 
     }
 };
+
+
+// 1 pass 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* temp = head;
+
+        for(int i = 1;i<=n; i++){
+            temp = temp->next;
+        } 
+        if(temp == NULL){
+            ListNode* res = head->next;
+            delete(head);
+            return res;
+        }
+        ListNode* prev  = head;
+
+        while(temp != NULL && temp->next != NULL){
+         prev = prev->next;
+         temp = temp->next;
+        }
+      
+       ListNode* del = prev->next;
+       prev->next = prev->next->next;
+       delete(del);
+
+       return head;
+    }
+};
