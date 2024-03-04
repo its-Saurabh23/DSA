4aeedd13941e3a9a27fde781b83c7807e47c704a
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-03-04 22:02:55 +0530

    new Array Question

---
+++ DSA Question 2024/Monotonic_Array.cpp
@@ -0,0 +1,23 @@
+   bool monotonicDecreasing(vector<int>&nums){
+          int n = nums.size(); 
+          for(int i = 1;i<n;i++){
+              if(nums[i]>= nums[i-1])continue;
+              else
+              return false;
+          }
+          return 1;
+    }
+
+    bool monotonicIncreasing(vector<int>&nums){
+          int n = nums.size(); 
+          for(int i = 1;i<n;i++){
+              if(nums[i]<= nums[i-1])continue;
+              else
+              return false;
+          }
+          return 1;
+    }
+    bool isMonotonic(vector<int>& nums) {     
+     return monotonicIncreasing(nums) || monotonicDecreasing(nums);
+    
+    }
