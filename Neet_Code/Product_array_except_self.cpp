fae467d0572ee6b0d8bd503fbc5dfcff5c690d13
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-03-16 12:52:39 -0400

    new update

--- Neet_Code/Product_array_except_self.cpp
+++ Neet_Code/Product_array_except_self.cpp
@@ -22,3 +22,41 @@ public:
 };
 
 // by division metod
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    
+    int count_of_zero = 0;
+    int product_without_zero = 1;
+    for(int &i: nums){
+        if(i == 0){
+            count_of_zero++;
+        }
+        else{
+            product_without_zero *= i;
+        }
+    }
+
+    vector<int>ans(n);
+    for(int i = 0;i<n; i++){
+        int num = nums[i];
+        if(num != 0){
+            if(count_of_zero >0){
+                ans[i] = 0;
+            }else{
+                ans[i] = product_without_zero / nums[i];
+            }
+        }else{
+            // num == 0
+            if(count_of_zero >1){
+                ans[i] = 0;
+            }
+            else{
+                ans[i] = product_without_zero;
+            }
+        }
+    }
+    return ans;
+    }
+};
