40cb621abd53948b82eebb33c95937141d535d47
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-03-16 13:02:10 -0400

    new update

--- Neet_Code/Product_array_except_self.cpp
+++ Neet_Code/Product_array_except_self.cpp
@@ -60,3 +60,35 @@ public:
     return ans;
     }
 };
+
+
+
+// Usig extra space 
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> right(n);
+    vector<int> left(n);
+    
+    left[0] = 1;
+    right[n-1] = 1;
+
+//   left
+    for(int i = 1;i<n; i++){
+       left[i] = nums[i-1] * left[i-1];
+    }
+
+// Right
+    for(int i = n-2; i>=0; i--){
+       right[i] = nums[i+1] * right[i+1];
+    }
+ 
+ vector<int>ans(n);
+
+ for(int i = 0; i<n ;i++){
+    ans[i] = left[i]*right[i];
+ }
+   return ans;
+ }
+};
