886a9f6122c6fcf91228419cdeccca1fd4f4eedb
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-03-18 13:09:48 -0400

    new question

--- java_2024/Merge Strings Alternately.java
+++ java_2024/Merge Strings Alternately.java
@@ -1,7 +1,7 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         StringBuilder sb = new StringBuilder();
-
+ 
         boolean flag = true;
         int i = 0;
         int  j = 0;
