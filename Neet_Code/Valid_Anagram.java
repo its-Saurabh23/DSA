fe46a6823766816107a673b374d270c260034719
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-05-02 14:53:55 -0400

    Update Valid_Anagram.java
    cpp code optimal one

--- Neet_Code/Valid_Anagram.java
+++ Neet_Code/Valid_Anagram.java
@@ -19,3 +19,37 @@ class Solution {
         return true;
     }
 }
+
+
+
+//Optimal one
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> res;
+
+        for(const string& ele : strs) {
+            // Count frequency of characters
+            int freq[26] = {0};
+            for(char val : ele) {
+                freq[val - 'a']++;
+            }
+
+            string key;
+            for(int i = 0; i < 26; i++) {
+                if(freq[i] != 0) {
+                    key += string(1, 'a' + i) + to_string(freq[i]);
+                }
+            }
+            mp[key].push_back(ele);
+        }
+
+        // Copy values of map to result
+        for(auto& kvp : mp) {
+            res.push_back(kvp.second);
+        }
+
+        return res;
+    }
+};
