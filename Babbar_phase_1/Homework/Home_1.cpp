458567424d5041ff7717b342fff996cadc99a05b
its-saurabh26 <ramdayalrajak25@gmail.com>
2024-05-30 02:37:55 -0400

    Update Home_1.cpp
    Remove the Unnecessary Space in the code and Beautify the code.

--- Babbar_phase_1/Homework/Home_1.cpp
+++ Babbar_phase_1/Homework/Home_1.cpp
@@ -1,29 +1,28 @@
 // Sum of arry
 
-
 #include<iostream>
 using namespace std;
 
-
 int arraySum(int arr[],int n){
 int sum = 0;
-for( int i =0;i<n; i++){
-    sum =sum+arr[i];
-}
+for( int i = 0; i<n; i++){
+    sum = sum + arr[i];
+ }  
 return sum;
 }
+
 int main(){
 int n;
 int arr[5];
 cout<<"Enter the size of Array "<<endl;
 cin>>n;
 cout<<"Enter the Elements of array "<<endl;
+    
 for (int i =0;i<n; i++){
     cin>>arr[i];
 }
+    
 int result = arraySum(arr,n);
 cout<<"Sum of the Arry is = " <<result<<endl;
-
-
     return 0;
 }
