59e8d8931122cbeef3bda1ebf1f32d402a0fef80
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-09-24 17:25:12 +0530

    triangle pattern

---
+++ Babbar_phase_1/triangle_pattern.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    /*
+        for (int i = 0; i < n; i++)   // row
+        {
+            for (int j = 0; j <i+1; j++)  // col
+            {
+                cout << " * ";
+            }
+            cout << endl;
+        }
+
+     // output
+     *
+     *  *
+     *  *  *
+     *  *  *  *
+     *  *  *  *  *
+
+
+
+
+     */
+
+    /*
+       for (int i = 0; i < n; i++)
+       { // row
+           for (int j = 0; j < n - i; j++)
+           { // col
+               cout << " * ";
+           }
+           cout << endl;
+       }
+   // output
+
+    *  *  *  *  *
+    *  *  *  *
+    *  *  *
+    *  *
+    *
+   */
+
+/*
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < i + 1; k++)
+        {
+            cout << " * ";
+        }
+        cout << endl;
+    }
+
+    // output 
+      *       
+     *  *     
+    *  *  *   
+   *  *  *  * 
+  *  *  *  *  *  
+*/
+
+  
+  for(int i=1; i<=n; i++){
+    
+    for(int j=1; j<=n-i; j++){
+      cout<<"  ";   //space
+    }
+
+    for(int k=1; k<=i;k++){
+        cout<< "* ";
+    }
+    cout<<endl;
+  }
+    return 0;
+}
