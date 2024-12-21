e704de229c9cf896f232b264a482372209198775
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-12-21 12:44:27 +0530

    recursion code updated.

--- Recursion/Sum_SubSequence.cpp
+++ Recursion/Sum_SubSequence.cpp
@@ -1,11 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//  -- if u want to print only 1 subSequence 
+bool flag = true;   
+
 void PrintSum(int idx,vector<int>&ds,int s,int arr[],int sum,int n){
    
    if(idx>= n){
-    if(s == sum){
+    if(s == sum && flag){
       for(auto it: ds)cout<< it <<" ";
+       
+      //  flag = false;
       cout<<endl;
     }
     return;
@@ -32,4 +38,54 @@ int main()
   
     PrintSum(0,ds,0,arr,sum,n);
     return 0;
+}
+
+
+// without the global variable 
+#include<bits/stdc++.h>
+using namespace std;
+
+int printSingleSubSequence(int idx,int n,int arr[],vector<int>&ds,int sum,int initalSum){
+  
+  if(idx >=n){
+    if(initalSum == sum){
+      for(auto it:ds)std::cout << it<<" ";
+      cout<<endl;
+       return true;
+    }
+    else{
+    return false;
+    }
+  }
+  
+  ds.push_back(arr[idx]);
+  initalSum += arr[idx];
+  if(printSingleSubSequence(idx+1,n,arr,ds,sum,initalSum) == true){
+    return true;
+  }
+  
+  ds.pop_back();
+  initalSum -= arr[idx];
+  
+  if(printSingleSubSequence(idx+1,n,arr,ds,sum,initalSum) == true){
+     return true; 
+  }
+  
+  return false;
+}
+
+int main(){
+  
+  int arr[]={1,2,1};
+  int n = 3;
+  int sum = 2;
+  vector<int>ds;
+  
+  if(printSingleSubSequence(0,n,arr,ds,sum,0)){
+   cout<<"SubSequence avuiable"<<endl;
+  }else{
+    cout<<"No SubSequence avuiable"<<endl;
+  }
+  
+  return 0;
 }
