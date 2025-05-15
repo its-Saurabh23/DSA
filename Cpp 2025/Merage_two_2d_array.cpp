b6cf2bb7f4d3c07c2c2f4034b38eaf6f3cdfdd86
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-05-15 14:31:39 +0530

    unordered_map Apporach

--- Cpp 2025/Merage_two_2d_array.cpp
+++ Cpp 2025/Merage_two_2d_array.cpp
@@ -36,3 +36,35 @@ public:
 
     }
 };
+
+
+
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        
+        unordered_map<int,int>mp;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        
+        for(int i = 0; i<n1; i++){
+            int id = nums1[i][0];
+            int value = nums1[i][1];
+            mp[id] += value;   
+        }
+        for(int j = 0; j<n2; j++){
+            int id = nums2[j][0];
+            int value = nums2[j][1];
+            mp[id] += value; 
+        }
+
+        vector<vector<int>>ans;
+
+        for (const auto& [id, value] : mp) {
+            ans.push_back({id, value});
+        }
+    
+      sort(begin(ans),end(ans));
+      return ans;
+    }
+};
