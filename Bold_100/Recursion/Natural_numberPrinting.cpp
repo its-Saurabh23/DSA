b5e689d873401d1e41d875981abc987d06cb7fc2
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-08-27 23:12:27 +0530

    8/27/25

---
+++ Bold_100/Recursion/Natural_numberPrinting.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void print(int n,int i){
+
+//     //base case
+//     if(i == n){
+//         cout<<i<<endl;
+//         return;
+//     } 
+//     cout<<i<<endl;
+//     print(n,i+1);
+// }
+
+
+// with single argument
+
+void print(int n){
+
+    //base case
+    if(n<1){
+        return;
+    } 
+    print(n-1);
+    cout<<n<<endl;
+}
+
+int main(){
+    int n;
+    cout<<"Enter a natural number"<<endl;
+    cin>>n;
+    // print(n,1);
+    print(n);
+    return 0;
+}
