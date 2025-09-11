581961c97a76a0411fbe9ddfa05e7e24680fc33d
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-09-11 13:35:58 +0530

    9/11/25

---
+++ Bold_100/Recursion/countVowle.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countVol(string str,int index){
+    
+    if(index == -1){
+        return 0;
+    }  
+    
+    if(str[index] =='a' || str[index] == 'e' ||str[index] =='i' || str[index] =='o' || str[index] == 'u'){
+        
+    return 1 + countVol(str,index-1);    
+        
+    }else{
+        return countVol(str,index-1);
+    }
+    
+} 
+
+
+int main(){
+   string str ="hii how are u i am fine";
+   int size = str.size();
+    
+    // cout<<str.size();
+    
+  int res = countVol(str,size);
+  cout<<res<<"\n";
+    
+    // cout<<"Hello"<<"\n";    
+    return 0;
+}
