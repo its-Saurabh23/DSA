c2bbe616e139264a764bffca190b9ca7b1c9ea56
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-09-26 23:37:44 +0530

    9/26/25

---
+++ Bold_100/Recursion/TowerOfHanoi.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void towerOfHanoi(int n,char from_rod,char to_rod,char aux){
+
+    // Base case 
+    if(n == 0)return;
+}
+
+int main(){
+    int N = 3;
+    towerOfHanoi(N,'A','C','B');
+    return 0;
+}
