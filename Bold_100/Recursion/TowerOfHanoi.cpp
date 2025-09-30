29825ae054afc61f3dc2932a15bf2befa060e678
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-09-30 21:57:56 +0530

    9/30/25

--- Bold_100/Recursion/TowerOfHanoi.cpp
+++ Bold_100/Recursion/TowerOfHanoi.cpp
@@ -6,7 +6,8 @@ void towerOfHanoi(int n,char from_rod,char to_rod,char aux){
     // Base case 1
     if(n == 0)return;
     towerOfHanoi(n-1,from_rod,aux,to_rod);
-    cout<<""
+    cout<<"move disk"<<n<<"from rod"<<from_rod<<"to road"<<to_rod<<;
+    towerOfHanoi(n-1,aux,to_rod,from_rod);
 
 }
 
