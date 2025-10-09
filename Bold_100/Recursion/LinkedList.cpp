b6d2ebd9a04d44c76e78c2f6a792203c84050c9b
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-10-09 17:17:59 +0530

    10/9/25

--- Bold_100/Recursion/LinkedList.cpp
+++ Bold_100/Recursion/LinkedList.cpp
@@ -1,71 +1,119 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node {
-   public:
-   int data;
-   Node* next;
-};
- 
-
-Node* reverorder(Node * head){
+class Node{
+  public:
+  int data;
+  Node* next;
   
+};
+
+Node* deleteInLinkList(Node* head,int node_to_delete){
+    
+       if(head == NULL){
+           return NULL;
+       }
+       
+       if(head->data == node_to_delete){
+           Node* temp  = head;
+           head = head->next;
+           
+           delete temp;
+           return head;
+       }
+       
+       Node* temp = head;
+       Node* prev = NULL;
+       
+       while(temp != NULL && temp->data != node_to_delete){
+           prev = temp;
+           temp = temp->next;
+       }
+        
+        if(temp == NULL){
+            cout<<"Node to delete Does not exit "<< node_to_delete << "\n";
+            return NULL;
+        }
+       
+        prev->next = temp->next; 
+        
+        delete temp;
+        
+        return head;
+}
+
+Node* reverse_LinkedList(Node* head){
     Node* prev = NULL;
     Node* curr = head;
     Node* next = NULL;
-
+    
     while(curr != NULL){
-      // save next Poinete
-      next = curr->next;
-
-      //reverse the nest Pointer;
-      curr->next = prev;
-
-      //move Pointers;
-      prev = curr;
-      curr = next; 
+        next = curr->next;
+        curr->next = prev;
+        
+        prev = curr;
+        curr = next; 
     }
     return prev;
-
 }
-
-void printList_recursion(Node* head){
-     
-    if(head == NULL){
-        return;
+ 
+void printMid(Node* head){
+    Node* slow = head;
+    Node* fast = head->next;
+    
+    while(fast != NULL){
+        fast = fast->next->next;
+        slow = slow->next;
     }
-    printList_recursion(head->next);
-    cout<<head->data <<" --> ";
-}
+    cout<<"Mid of the Link list "<<slow->data<<"\n";
+} 
 
-void printList(Node* head){
-  Node* temp = head; 
-  while(temp != nullptr){
-      cout<<temp->data<<" --> ";
-      temp = temp->next;
-  }cout<< "NULL "<<"\n"; 
-}
 
-void push(Node* &head, int new_data){
+void makeLinkedList(Node* &head,int new_data){
     Node* new_Node = new Node();
-    new_Node->data = new_data;
-    new_Node->next = head;
+    new_Node->data = new_data;  
+    new_Node->next = head;    
     head = new_Node;
 }
 
-int main(){
-  Node* head = NULL;
-  push(head,1);
-  push(head,2);
-  push(head,3);
-  cout<<"Original List"<<"\n";
-  printList(head);
-//   cout<<"Reversed order Linked list"<<"\n";
-  
-//   head = reverorder(head);
-//   printList(head);  
-
-  cout<<"Reverse Linked List By Recursion "<<"\n";
-  printList_recursion(head);
-  return 0; 
+void print(Node* head){
+  Node* temp = head;
+  while(temp != NULL){
+       std::cout << temp->data <<"--> ";
+       temp = temp->next;
+  }cout<<"NULL";
 }
+
+
+int main(){
+    Node* head = NULL;
+    
+    makeLinkedList(head,10);
+    makeLinkedList(head,20);
+    makeLinkedList(head,30);
+    makeLinkedList(head,40);
+    makeLinkedList(head,50);
+    cout<<"Linked List traversal"<<"\n";
+    cout<<"\n";
+    print(head);
+    // cout<<"\n";
+    // cout<<"\n";
+    
+    // cout<<"Mid of the Linked List"<<"\n";
+    // printMid(head);
+    
+    // cout<<"\n";
+    // cout<<"\n";
+    // cout<<"Reverse Linked List"<<"\n";
+    // head = reverse_LinkedList(head);
+    // print(head);
+    // cout<<"\n";
+    cout<<"\n";
+    cout<<"Delete in Link list"<<"\n";
+    head = deleteInLinkList(head,30);
+    cout<<"\n";
+    cout<<"Print after Deletion in linked list"<<"\n";
+    print(head);
+    
+    return 0;
+ }
