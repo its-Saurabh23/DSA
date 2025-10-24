b4a6c81bff4a59bdf6ce44f7673a8e1891842707
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-10-24 22:14:01 +0530

    10/24/25

--- Bold_100/Recursion/Flattening_link_list.cpp
+++ Bold_100/Recursion/Flattening_link_list.cpp
@@ -16,20 +16,60 @@ class Node{
 
 Node * Flattening(Node* head, Node bottom){
     vector<int>result;
+    Node* temp = bottom;
 
-    Node* temp;
-    while(temp != NULL){
-        
+    while(head != NULL){
+        result.push_back(head->data);
+        while(temp != NULL){
+           result.push_back(temp->data);
+           temp = temp->next; 
+        }
+        head = head->next;
     }
+
+    // Sorting
+    sort(result.begin(),result.end());
+
+    // creation of new Link list
+   
+    Node* head = NULL;
+    Node* tail  = NULL;
     
+    for(int i = 0; i<result.size(); i++){
+        // create a new node 
+        Node* temp = new Node(result[i]);
+       
+        // create head
+        if(head == NULL){
+            head = newNode;
+        }else{ 
+            tail->bottom = newNode;
+        }
+        tail = newNode;
+    }
+    return  head;
+    
+}
+ 
+void print(Node* head){
+     
+    while(temp != NULL){
+
+       cout<<temp->data<<"-> ";
+       temp = temp->next;
+
+    }cout<<"NULL"<<"\n";
 }
 
 int main(){
     Node* head = new Node(1);
     head->next = new Node(2);
-    
-    head->Bottom = new Node(1.1);
+    head->next->bottom = new Node(2.1);
 
+    head->next->next->next = new Node(3);
+    head->next->next->next->bottom = new Node(3.1);
+    head->next->next->next->bottom->bottom = new Node(3.2);
+    head->next->next->next->bottom->bottom->bottom = new Node(3.3);   
     return 0;
  
  
