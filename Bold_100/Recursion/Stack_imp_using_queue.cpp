7ee6768240c83aa435645144794251820b71ebaf
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-11-18 21:31:55 +0530

    Update Stack_imp_using_queue.cpp

--- Bold_100/Recursion/Stack_imp_using_queue.cpp
+++ Bold_100/Recursion/Stack_imp_using_queue.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 class Queue{
    public:
+   queue<int>q;
 
+void push(int val){
+     int size = q.size();
+      for(int i= 0 ; i<size;i++){
+            q.push(q.front());
+                q.pop();
+          }
+    }
+int pop(){
+    if(is empty()){
+        cout<<"Stack is overflow";
+               return -1;
+      }
+   
+     int temp = q.front();
+                q.pop();
+
+       return temp;
+ }
    
 };
 
