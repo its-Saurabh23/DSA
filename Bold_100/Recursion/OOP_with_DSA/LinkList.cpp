617a22c92ec80ef556563a8567c962696d1332ae
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-12-03 18:33:52 +0530

    12/3/25

---
+++ Bold_100/Recursion/OOP_with_DSA/LinkList.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class House{
+  public:
+  
+  string address;
+  string owner;
+  int rooms;
+
+  House(string o, string a,int r){
+    : owner(o),address(a),rooms(r){}
+  }
+};
+
+// -------------------- NODE TEMPLATE --------------------
+ 
+template<typename T>
+
+class Node{
+     T data;
+     Node* next;
+
+     Node(T d): data(d),next(nullptr) {}
+};
+
+// -------------------- Linke List TEMPLATE --------------------
+
+template <typename T>
+
+class LinkList{
+
+    public:
+    Node<T>* head;
+
+    LinkedList():head(nullptr){}
+    
+};
+
+
+int main(){
+
+    result 0;
+}
