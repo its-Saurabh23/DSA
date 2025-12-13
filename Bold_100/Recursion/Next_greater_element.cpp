2944ba635300c6fc8ce9d66699b81cf04e4f45d3
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-12-13 16:40:02 +0530

    complete NEext Greater Element

--- Bold_100/Recursion/Next_greater_element.cpp
+++ Bold_100/Recursion/Next_greater_element.cpp
@@ -1,16 +1,42 @@
 #include<bits/stdc++.h>
-using namespaces std; 
-
-int Nextgreate(vector<int>& nums){
-
-vector<int>result;
+using namespace std;
 
 
-
+vector<int>NextGreaterElement(vector<int>&nums){
+    vector<int>result;
+    
+    if(nums.empty()){
+        std::cout<<"Empty input"<<"\n";
+        return {};
+    }
+    
+    stack<int>st;
+    
+    for(int i = nums.size()-1; i>=0; i--){
+        
+        while(!st.empty() && st.top() <= nums[i]){
+            st.pop();
+        }     
+       
+       if(st.empty()){
+           result.push_back(-1);
+       }else{
+           result.push_back(st.top());
+       }
+       
+       st.push(nums[i]);
+    }
+    reverse(result.begin(), result.end());
+    
+    return result;
 }
-int main(){
-vector<int>nums={42,4,65,3,4};
 
-Nextgreate(nums);
-return 0;
+int main(){
+    vector<int>num={21,2,44,5,1,9};
+    vector<int>ans = NextGreaterElement(num);
+    
+    for(int x: ans){
+        std::cout<<x<<"\t";
+    }
+    return 0;
 }
