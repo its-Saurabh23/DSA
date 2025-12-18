dcc257a6d359b02fb89e95144683233d5731b1e4
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-12-18 19:04:22 +0530

    12/18/25

--- Bold_100/Recursion/GDC.cpp
+++ Bold_100/Recursion/GDC.cpp
@@ -1,16 +1,14 @@
  #include<bits/stdc++.h>
  using namespace std;
  
- 
  int GCD(int num1,int num2){
      
      if(num2 == 0){
           return num1;
      }
+
     return GCD(num2,num1%num2); 
  }
- 
- 
  
  int main(){
      
@@ -19,8 +17,6 @@
      std::cin >> num1;
      cout<<"ENter another Number"<<endl;
      cin>>num2;
-     
-     
      int result = GCD(num1,num2);
      cout<<"GCD of given Number "<<result<<endl;
      return 0;
