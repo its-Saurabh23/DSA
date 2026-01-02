d2b05359b14f04e5ffe3d4d14443c958ce806a3a
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-01-02 19:10:20 +0530

    1/2/26  knapck is runing

--- Bold_100/knapsack_DP.cpp
+++ Bold_100/knapsack_DP.cpp
@@ -1,15 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
-bool knapsack (){
+int kanpsnack(vector<int>&wt,vector<int>&arr,int W,int n){
+   
+    //Base Case
+    if(n == 0 || W == 0){
+       return 0;
+    }
 
+    //choice Diagram
+    if(wt[n-1]>w){
+    return kanpsnack(wt,arr,w,n-1);
+    }
+    else  if(wt[n-1]<=w){
+        // 2 case pick or non pick
+    }
 }
+
 int main (){
 
-int wt = [];
-int val = [];
-int w = 10;
-knapsack(wt,val,W,n);
+    int wt = [];
+    vector<int>wt={}
+    vector<int>val={}
+    int W = 10
+    int n = val.size();
+
+kanpsnack(wt,val,W,n);
 return 0;
 
 }
