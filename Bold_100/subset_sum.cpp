7239513539212cd4a2d6bb26c3751229c85c45c2
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-01-06 19:13:28 +0530

    1/6/26

--- Bold_100/subset_sum.cpp
+++ Bold_100/subset_sum.cpp
@@ -1,7 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool subSetSum(vector<int>&arr,int sum,int n){
+   
+    if(sum == 0){
+      return true;
+    }
+    if(n === 0){
+        return false;
+    }
+
+    if(){
+
+    }
+}
+
 int main(){
 
-return 0;
+   vector<int>arr={1,2,3,4,};
+   int sum = 10;
+   int n = arr.size();
+   subSetSum(arr,sum,n);
+   return  0;
 }
