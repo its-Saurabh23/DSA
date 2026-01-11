6fca369f867526b966181e0caffb76e8cc1454d4
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-01-11 18:26:31 +0530

    Update count_of_subsetSum_with_given_sum.cpp
    complete 11/1/26

--- Bold_100/count_of_subsetSum_with_given_sum.cpp
+++ Bold_100/count_of_subsetSum_with_given_sum.cpp
@@ -1,16 +1,38 @@
-#include<bits/stdc++.h>
+ #include <bits/stdc++.h>
 using namespace std;
 
-int CountofSubsetSum(vector<int>&arr,int sum ,int n){
- if(sum == 0){
-   return 1,;
-  }
-  if(n == 0){
-   return 0
- }
- return 
- }
+int t[1001][1001];   // DP table
 
-int main(){
-  return 0;
-  }
+int CountofSubsetSum(vector<int> &arr, int sum, int n) {
+    // Base cases
+    if (sum == 0)
+        return 1;
+
+    if (n == 0)
+        return 0;
+
+    if (t[n][sum] != -1)
+        return t[n][sum];
+
+    // Not take current element
+    int notTake = CountofSubsetSum(arr, sum, n - 1);
+
+    // Take current element (only if possible)
+    int take = 0;
+    if (sum >= arr[n - 1])
+        take = CountofSubsetSum(arr, sum - arr[n - 1], n - 1);
+
+    return t[n][sum] = take + notTake;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 3};
+    int sum = 6;
+    int n = arr.size();
+
+    memset(t, -1, sizeof(t));
+
+    cout << "Count of subsets = " << CountofSubsetSum(arr, sum, n);
+
+    return 0;
+}
