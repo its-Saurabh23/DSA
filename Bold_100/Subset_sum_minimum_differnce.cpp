d7f8c0defe12a5e219961055c42de34b7b906c85
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-01-12 15:46:40 +0530

    1/12/26

---
+++ Bold_100/Subset_sum_minimum_differnce.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int subSetSum(vector<int>&arr,int m int sum){
+    if(n == 0){
+      return 0;
+    }
+    if(sum == 0){
+       return 1;
+    }
+
+    // 
+}
+
+int solve(vector<int>&arr){
+  
+    int n = ar.size();
+    int sum = 0;
+    for(int i  = 0;i <n; i++)}{
+        sum += arr[i];
+    }
+
+    subSetSum(arr,n,sum);
+
+}
+
+int main(){
+  vector<int>arr={1, 6, 11, 5};
+  solve(arr);
+    return 0;
+}
