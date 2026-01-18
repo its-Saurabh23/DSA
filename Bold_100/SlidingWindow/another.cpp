00733cb11c290a71a85459206f0bf561f26c45b7
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-01-18 20:01:28 +0530

    Update another.cpp
    longest substring in a string

--- Bold_100/SlidingWindow/another.cpp
+++ Bold_100/SlidingWindow/another.cpp
@@ -1,8 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+int solve (string str){
+     if(str.length== 0){
+        return 0;
+      }
+  } 
 int main(){
 
-
+string str="abcabcabb";
+solve(str);
 return 0;
 }
