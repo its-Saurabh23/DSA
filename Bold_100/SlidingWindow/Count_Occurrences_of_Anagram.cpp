0e13d45ac5f412d5f18333e833844ebfc7847780
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-01-28 22:44:30 +0530

    Update Count_Occurrences_of_Anagram.cpp correct one

--- Bold_100/SlidingWindow/Count_Occurrences_of_Anagram.cpp
+++ Bold_100/SlidingWindow/Count_Occurrences_of_Anagram.cpp
@@ -1,46 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isAnagram(string &str, string &word){
-  vector<int>arr(26,0);
+bool isAnagram(string &str, string &word) {
+    if (str.length() != word.length()) return false;
 
- for(char c:str)arr.push_back(str['a'-c])++;
- for(char c:word)arr.push_back(word'a'-c])--;
+    vector<int> arr(26, 0);
 
-  for(int i=0;i<arr.size(); i++){
-        if(arr[i]>1){
-           return false;
-          }
-      }
-   return true;
+    for (char c : str) {
+        arr[c - 'a']++;
+    }
 
+    for (char c : word) {
+        arr[c - 'a']--;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (arr[i] != 0) {
+            return false;
+        }
+    }
+    return true;
 }
-int solve(string &text, string &word){
 
-  string str = "";
-  int start = 0;
-  int len = word.length();
-  int count= 0;
+int solve(string &text, string &word) {
+    string str = "";
+    int start = 0;
+    int len = word.length();
+    int count = 0;
 
-  for(int i = 0; i<text.length(); i++;){
-         str += text[i];
-        if(i-strat +1 == len ){
-            if((str,word)){
-              count++;
-              start++;
-             }
-            str = str - str[i-start-1];
-          }
-      
-     }
-    
+    for (int i = 0; i < text.length(); i++) {
+        str += text[i];
+
+        if (i - start + 1 == len) {
+            if (isAnagram(str, word)) {
+                count++;
+            }
+            str.erase(0, 1);  // remove first char
+            start++;
+        }
+    }
+    return count;
 }
-int main(){
-   
-  string text = "gotxxotogatog";
-  string word = "got";
-  int count = solve(text, word);
-   cout<<count<<endl;
-    
+
+int main() {
+    string text = "gotxxotogatog";
+    string word = "got";
+
+    int count = solve(text, word);
+    cout << count << endl;
+
     return 0;
 }
