bfbb1f095a487570c3fc7870e161523ec5d16047
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-02-13 15:09:47 +0530

    Count Sub arrays having Sum K

---
+++ Bold_100/ArrayBasics/CountSubarrayshavingSumK.cpp
@@ -0,0 +1,13 @@
+#include<bits/stc++.h>
+using namespace std;
+
+int CountSubarrayshavingSumK(vector<int>&arr,int k){
+   
+} 
+
+int main(){
+    vector<int>arr={10,2,-2,-20,10};
+    int k = -10;
+    CountSubarrayshavingSumK(arr,k);
+    return 0;
+}
