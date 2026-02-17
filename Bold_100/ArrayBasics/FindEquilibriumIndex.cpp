f856642a397f0c349046d90bbd430fbed0ff88ef
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-02-17 11:01:56 +0530

    Find Equilibrium indix of an array Prefix sum thought process

---
+++ Bold_100/ArrayBasics/FindEquilibriumIndex.cpp
@@ -0,0 +1,30 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int findEquilibriumIndex(vector<int>&arr){
+    long long totalSum = 0;
+
+    for(int x : arr){
+        totalSum += x; 
+    }
+    cout<<"totalSum "<<totalSum<<"\n";
+    
+    long long leftSum = 0;
+    for(int i = 0; i<arr.size(); i++){
+       
+        if(leftSum == totalSum - leftSum - arr[i]){
+            return i;
+        }
+        leftSum += arr[i];
+    }
+    return -1;
+}
+
+int main() {
+    // Write C++ code here
+    std::cout << "Try programiz.pro"<<"\n";
+    vector<int>arr={-7, 1, 5, 2, -4, 3, 0};
+    int result = findEquilibriumIndex(arr);
+    std::cout<<result<<"\n";
+    return 0;
+}
