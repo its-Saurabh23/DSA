1e0febf8694784a1f6128b0d88d2a2b6fe5d4339
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-03-08 21:37:23 +0530

    Update Binary_search.cpp remove unnecessary comments

--- DSA/Binary_search.cpp
+++ DSA/Binary_search.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int  BinarySearch(int arr[],int n,int target){
 
-int s =0;
-int e =n-1;
+int s = 0;
+int e = n-1;
 int mid = s +(e-s)/2;
 
 while(s<=e){
@@ -14,14 +14,13 @@ while(s<=e){
         return mid;
     }
 
-    //Condition Second 
     if(arr[mid]<target){
         s = mid+1;
     }
     else{
         e = mid-1;
     }
-    mid = s+(e-s)/2;
+    mid = s + (e-s)/2;
 }
 return s;
 }
