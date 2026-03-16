a657de163d9e0f569698a407d53b3b05d3799c61
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-03-16 11:14:26 +0530

    Practice to Move all zero to end brute force and inplace

--- Bold_100/ArrayBasics/MoveAllZerosToEnd.cpp
+++ Bold_100/ArrayBasics/MoveAllZerosToEnd.cpp
@@ -26,3 +26,56 @@ int main()
 
     return 0;
 }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void MoveAllZeros(vector<int>arr,int n){
+  
+  vector<int>temp(n);
+  
+  int j = 0;
+  for(int i= 0; i<n; i++){
+      
+      if(arr[i] != 0){
+        temp[j++] = arr[i];
+      }
+  }
+   
+   while(j<n){
+       temp[j++] = 0;
+   }
+   
+   
+   for( auto &x : temp){
+       cout<<x<<" ";
+   }
+    
+}
+
+
+void MoveAllZerosInplace(vector<int>&arr){
+    
+    int j = 0;
+    for(int i = 0; i<arr.size(); i++){
+        
+        if(arr[i]!= 0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+    
+}
+
+int main()
+{
+    vector<int>arr={1, 2, 0, 4, 3, 0, 5, 0};
+    int n = arr.size();
+    // MoveAllZeros(arr,n);
+    MoveAllZerosInplace(arr);
+    for(auto &x:arr){
+        cout<<x<<" ";
+    }
+    return 0;
+}
