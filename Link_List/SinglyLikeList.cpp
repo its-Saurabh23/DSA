73f4097f650df25fb8dae9bb5f6eb74f118ba11b
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-03-21 16:33:24 +0530

    Practice Link list

---
+++ Link_List/SinglyLikeList.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+  
+  public:
+  int data;
+  Node* next;
+  
+  Node(int d){
+      data = d;
+      next = NULL;
+  }
+};
+
+class LinkList {
+  
+  private:
+  Node* head;
+  
+  public:
+   LinkList(){
+    head = NULL;   
+   }
+   
+   void createNode(int val){
+       Node* newNode = new Node(val);
+       if(head == NULL){
+           head = newNode;
+           return;
+       }
+
+       Node* temp = head;
+       while(temp->next != NULL){
+           temp = temp->next;
+       }
+       temp->next = newNode;
+   }
+   
+   void deleteNode(int val){
+       
+       if(head == NULL){
+           cout<<"List is empty";
+           return;
+       }
+       if(head->data == val){
+          Node* temp = head;
+          head = head->next;
+          delete temp;
+          return;
+       }
+       
+       Node* temp = head;
+       while(temp->next != NULL && temp->next->data != val){
+           temp= temp->next;
+       }
+       if(temp->next == NULL){
+           return;
+       }
+       
+       Node* NodeTodelete = temp->next;
+       temp->next =temp->next->next; 
+       delete NodeTodelete;
+       
+   } 
+   
+   void dipslay(){
+       Node* temp = head;
+       
+       while(temp != NULL){
+           std::cout << temp->data <<" -> ";
+           temp= temp->next;
+       }
+       cout<<" NULL";
+   }
+   
+};
+
+int main(){
+    LinkList list;
+    list.createNode(10);
+    list.createNode(20);
+    list.createNode(30);
+    
+    cout<<"Link List before Deletion "<<"\n";
+    list.dipslay();
+    cout<<"\n";
+    cout<<"After Deletion in Link List"<<"\n";
+    list.deleteNode(20);
+    list.dipslay();
+    return 0;
+}
