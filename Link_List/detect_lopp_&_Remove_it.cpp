a0b07396acb28164c8227a609d7d4343d1295e9d
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-03-27 23:23:19 +0530

    Add loop detection and removal in linked list
    Implement function to detect and remove loop in linked list.

--- Link_List/detect_lopp_&_Remove_it.cpp
+++ Link_List/detect_lopp_&_Remove_it.cpp
@@ -22,4 +22,42 @@ class Solution
     }
 };
 
+
+void detchLoopInLinkList(Node* head){
+    
+    Node* slow = head;
+    Node* fast = head;
+    
+    bool hasLoop = false;
+    
+    while(fast != NULL && fast->next != NULL){
+        slow=slow->next;
+        fast = fast->next->next;
+        
+        if(fast == slow){
+            hasLoop = true;
+            break;
+        } 
+    }
+    if(!hasLoop){
+        cout<<"No Loop is Present"<<"\n";
+        return;
+    }
+    slow = head;
+    while(slow != fast){
+        slow = slow->next;
+        fast = fast->next;
+    }
+    cout<<"Loop start at node "<<slow->data<<"\n"<<endl;
+    Node* temp = slow;
+    while(temp->next != slow){
+        temp = temp->next;
+    }
+    temp->next= NULL;
+    cout<<"Loop rempove \n";
+    
+    
+    std::cout <<"Loop Node " <<slow->data<< std::endl;
+}
+
 */
