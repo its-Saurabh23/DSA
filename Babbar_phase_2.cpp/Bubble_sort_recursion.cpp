419a2dadce1f5d5c3a2530fad6eb487023d3d235
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-04-04 21:33:34 +0530

    Update Bubble_sort_recursion.cpp Change comment

--- Babbar_phase_2.cpp/Bubble_sort_recursion.cpp
+++ Babbar_phase_2.cpp/Bubble_sort_recursion.cpp
@@ -8,14 +8,13 @@ void sortArray(int *arr,int n){
         return;
     }
 
-
  for(int i =0; i<n-1; i++){
     if(arr[i]>arr[i+1]){
        swap(arr[i],arr[i+1]);
     }
  }
 
-// Recursive Call
+// Recursive function calling 
 sortArray(arr,n-1);
 }
 
