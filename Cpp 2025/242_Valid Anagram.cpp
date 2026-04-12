06e1665fbc57324c4b41355be5cf6e86f63e8d40
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-04-12 23:08:37 +0530

    Update 242_Valid Anagram.cpp valid anagram

--- Cpp 2025/242_Valid Anagram.cpp
+++ Cpp 2025/242_Valid Anagram.cpp
@@ -4,14 +4,12 @@ public:
      int n1 = s.length();
      int n2 = t.length();
 
-     if(n1 != n2)return false;
-
+     if(n1 != n2)return false;
      unordered_map<char,int>mp;
 
      for(char ch :s){
         mp[ch]++;
-     }
-
+     }
      for(char ch:t){
           mp[ch]--;
           if(mp[ch]<0){
