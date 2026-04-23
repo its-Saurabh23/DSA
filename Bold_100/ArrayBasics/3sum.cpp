36964d042eb7b3b6dd9091f7bcae822f2d5e167e
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-04-23 22:48:00 +0530

    Update 3sum.cpp in progress

--- Bold_100/ArrayBasics/3sum.cpp
+++ Bold_100/ArrayBasics/3sum.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 vector threeSum(vector<int>&nums){
    int n = nums.size();
+   
+for(int i= 0; i<n-2;i++){
+
+      int Target = - (nums[i]);
+     } 
  }
 int main(){
   return 0;
