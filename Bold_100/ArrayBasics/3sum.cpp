5785f1a14f5a0d6a6f4e9d7a29d643560ff6e9fa
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-04-24 23:15:26 +0530

    Update 3sum.cpp continue 3 sum

--- Bold_100/ArrayBasics/3sum.cpp
+++ Bold_100/ArrayBasics/3sum.cpp
@@ -1,12 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void twoSum(int n, vector<int>&nums,int Target,int i){
+
+   
+   }
+
 vector threeSum(vector<int>&nums){
    int n = nums.size();
    
 for(int i= 0; i<n-2;i++){
 
       int Target = - (nums[i]);
+        twoSum(n,nums, Target,i);
      } 
  }
 int main(){
