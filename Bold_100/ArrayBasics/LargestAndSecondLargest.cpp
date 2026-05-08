307253eaa01eada3e87e579e183bcd5a1b6f5e85
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-05-08 16:44:51 +0530

    Refactor to find second maximum in array

--- Bold_100/ArrayBasics/LargestAndSecondLargest.cpp
+++ Bold_100/ArrayBasics/LargestAndSecondLargest.cpp
@@ -6,31 +6,35 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-void largestSecondLargest(vector<int>&arr){
-    
-    int largest = INT_MIN;
-    int secodLargest = INT_MIN;
-    
-    for(int i =0; i<arr.size(); i++){
-        if(i>largest){
-            secodLargest = largest;
-            largest = arr[i];
-        }else if(i>secodLargest && secodLargest  != largest){
-            secodLargest = arr[i];
-        }
-    }
-    std::cout << "Largest " <<largest<< std::endl;
-    std::cout << "Second Larget " <<secodLargest<< std::endl;
+
+int SecondMAximum(vector<int>nums){
+   int firstMaximum =  INT_MIN;
+   int secondMaximum = INT_MIN;
+   
+   for(int i =0;i<nums.size(); i++){
+       
+       if(nums[i]>firstMaximum){
+           secondMaximum = firstMaximum;
+           firstMaximum = nums[i];
+       }else if(nums[i]>secondMaximum && nums[i] != firstMaximum){
+           secondMaximum = nums[i];
+       }
+   }
+   return secondMaximum;
 }
+
+
+
 
 
 int main()
 {
-    vector<int>arr={1,2,3,4,5,67,8,9,10};
-    largestSecondLargest(arr);
+    vector<int>nums = {1,2,3,4,5};
+     int result = SecondMAximum(nums);
+     std::cout << result << std::endl;
 
     return 0;
 }
