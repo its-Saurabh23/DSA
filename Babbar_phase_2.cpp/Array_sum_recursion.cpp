3084fbb45f824db1c83ae2963236408fc446789e
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-05-15 17:36:34 +0530

    Refactor getSum function for array summation

--- Babbar_phase_2.cpp/Array_sum_recursion.cpp
+++ Babbar_phase_2.cpp/Array_sum_recursion.cpp
@@ -1,29 +1,24 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
 
 int getSum(int arr[],int size){
 //  base case
-
 if(size == 0){
     return 0;
 }
+    
 if(size == 1){
     return arr[0];
 }
-
-
+    
 int sum = arr[0] + getSum(arr+1, size-1);
 return sum;
-
 }
+
 int main(){
     
     int arr[5]= {2,4,9,9,9};
-
     int size = 5;
     int sum = getSum(arr,size);
-
     cout<<"Sum is "<<sum <<endl;
 }
