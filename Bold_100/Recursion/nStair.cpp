8fca2280ded0cc827a79d91bc39248b42cfe0819
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-06-12 22:40:54 +0530

    Update nStair.cpp n staire

--- Bold_100/Recursion/nStair.cpp
+++ Bold_100/Recursion/nStair.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 
-int  totalStairCount(int n){
+int totalStairCount(int n){
     if(n<=1){
      return 1;
     }
+
     return totalStairCount(n-1) + totalStairCount(n-2);
+
 }
 int main(){
+
     int num;
     cin>>num;
+
     int result  = totalStairCount(num);
     cout<<result<<endl;
 }
