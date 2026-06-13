475be3d25e2af369b848a6a0915b38c107e1325b
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-06-13 23:41:52 +0530

    Update Array_sum_recursion.cpp remove unwanted space

--- Babbar_phase_2.cpp/Array_sum_recursion.cpp
+++ Babbar_phase_2.cpp/Array_sum_recursion.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int getSum(int arr[],int size){
+
 //  base case
 if(size == 0){
     return 0;
@@ -13,6 +14,7 @@ if(size == 1){
     
 int sum = arr[0] + getSum(arr+1, size-1);
 return sum;
+
 }
 
 int main(){
