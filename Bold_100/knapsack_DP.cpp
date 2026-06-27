1755ec77b121b910b534480e0b91ad68185049bc
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-06-27 11:28:11 +0530

    Correct syntax and enhance knapsack function
    Fix syntax errors and improve knapsack function implementation.

--- Bold_100/knapsack_DP.cpp
+++ Bold_100/knapsack_DP.cpp
@@ -27,6 +27,33 @@ int main (){
     int n = val.size();
 
 kanpsnack(wt,val,W,n);
-return 0;
+return 0
+}
 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int knapsack(int wt[],int val[],int W,int n){
+    
+    // Base case 
+    if(n == 0 || W == 0 ){
+        return 0;
+    }
+    
+    // choice digram 
+    if(wt[n-1]<=W){
+        
+     return max(val[n-1] + knapsack(wt,val,W-wt[n-1], n-1),knapsack(wt,val,W,n-1));
+    }
+    else if(wt[n-1]>W){
+        return knapsack(wt,val,W,n-1);
+    }
+    
+}
+
+int main()
+{
+ 
+    return 0;
 }
