c001bb407132c338f57b6c3052a642930f950b8e
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-07-11 17:40:12 +0530

    Implement recursive binary search function

--- Bold_100/Algo/BinarySearch.cpp
+++ Bold_100/Algo/BinarySearch.cpp
@@ -33,3 +33,47 @@ int binarySearch(int arr[],int n,int target){
     }
     return -1;
 }
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    // int BinarySearch(vector<int>&V,int target,int start,int end){
+    int BinarySearch(vector<int>&V,int target,int start,int end){
+
+        if(start > end){
+        //   return false;
+        return -1;
+        }
+        // it is index
+        int mid = start + (end-start)/2; 
+
+
+        if(V[mid]== target){
+            return mid;
+        }else if(V[mid] >target){
+            return BinarySearch(V,target,start,mid-1);
+        }else if(V[mid]<target){
+        return BinarySearch(V,target,mid+1,end);
+        }
+        // return false;
+        return -1;
+    }
+
+    int main(){
+        std::vector<int>V={1,2,3,4,5,6,7,8,9,10};
+        int size = V.size();
+        int target = 9;
+
+        int index = BinarySearch(V,target,0,size-1);
+        if(index != -1){
+            std::cout << "Number is present at index " << index << std::endl;
+        }else{
+            std::cout << "Number is not present" << std::endl;
+        }
+        // bool isPresent = BinarySearch(V,target,0,size-1);
+        // if(isPresent){
+        //     std::cout << "Number is present" << std::endl;
+        // }else{
+        //     std::cout << "Number is not present" << std::endl;
+        // }
+        return 0;
+    }
