9346077d5c12ca7d97692f2970d2d33c1ffa4f15
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-07-23 21:47:45 +0530

    Update AVLDeletion.cpp

--- Binary_tree/AVLDeletion.cpp
+++ Binary_tree/AVLDeletion.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node{
+public:
+int data;
+int height;
+Node* left;
+Node* right;
+
+Node(int value){
+
+ data = value;
+ height=1;
+ left =right=NULL;
+
+}
+
+};
 int main(){
 
 
