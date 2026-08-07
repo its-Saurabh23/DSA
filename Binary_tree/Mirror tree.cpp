ef4cd9e7fae8999ca04bee5e0355268ea0435730
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-08-07 17:11:41 +0530

    Update Mirror code in and its class

--- Binary_tree/Mirror tree.cpp
+++ Binary_tree/Mirror tree.cpp
@@ -1,22 +1,42 @@
 #incude<bits/stdc++.h>
 using namespace std;
-
+ 
 class Node{
 public:
+   int data;
+   int height;
+   Node* lefr ;
+   Node* right;
 
+   Node(int value){
+      this->data = value;
+     this->height = 1;
+     left = NULL;
+     right= NULL;
+   }
 };
 
 class Solution{
 public:
 
-bool solve(Node*root){
+
+// Mirror a tree and Invert a Tree;
+void solve(Node*root){
+
+  if(!root){
+   return; 
+  }
+  solve(root->left);
+  solve(root->rigt);
+  swap(root->left,root->right);
   
-
  };
-
 };
 
 int main(){
+  Node* root = NULL;
+  Solution obj;
+  obj.solve(root);
 
 return 0;
 }
