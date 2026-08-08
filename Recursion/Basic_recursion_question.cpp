c07fcbf0b0c6c105703849e7a5fc686fec706075
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-08-08 17:08:39 +0530

    Add basic recursion functions in Basic_recursion_question.cpp
    Implemented recursive functions for solving basic problems including printing numbers, calculating sum, factorial, and Fibonacci sequence.

---
+++ Recursion/Basic_recursion_question.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+
+    public:
+    void solve(int n,int i){
+        // base case 
+        if(i == n){
+            return;
+        }
+        cout<<i<<" ";
+        solve(n,i+1);    // Recursive call
+    };
+
+    void solve1(int n){
+        //base case
+        if(n == 0){
+            return;
+        }
+        solve1(n-1);   //Recursive call
+        cout<<n<<" ";
+    }
+
+    void SumN(int n,int &sum){
+        //base case
+        if(n == 0){
+            return;
+        }
+        sum += n;
+        SumN(n-1,sum); //Recursive call;
+    }
+
+    int factorial(int n){
+         //base case
+         if(n == 1){
+              return n;
+         }
+         return n * factorial(n-1); // recursive call;
+    } 
+
+    int fibonacci(int n){ 
+       if(n == 0){
+        return n;
+       }
+
+       if(n == 1){
+            return n;
+        return fibonacci(n-1) + fibonacci(n-2);  // recursive call;
+    }
+   }
+   
+};
+
+
+
+int main(){
+    
+    Solution obj;
+    int n = 10;
+    int i = 0;
+    
+    obj.solve(n,i); 
+    obj.solve1(n);
+
+    int sum = 0;
+    obj.sumN(n,sum);
+
+    int fact = 5;
+    int ans = obje.factorial(fact);
+    std::cout<<ans<<std::endl;
+    
+    int fibN = 5;
+    int fibAns = obj.fibonacci(fibN);
+    std::cout<<fibAns<<std::endl;
+
+    return 0;
+}
