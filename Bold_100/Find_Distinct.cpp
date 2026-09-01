1bb23d58d9b0707fde162180f54a083129f13678
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-09-01 21:56:47 +0530

    Update Find_Distinct.cpp remove unwanted space

--- Bold_100/Find_Distinct.cpp
+++ Bold_100/Find_Distinct.cpp
@@ -4,16 +4,13 @@ using namespace std;
 int main(){
 
    vector<int>V = {1,2,3,4,1,2,3,4,5,6,};
-
     long long xor_All = 0;
 
     for(const int num:V){
         xor_All ^= num;
     }
     
-     
     long long mask = xor_All & -xor_All;
-
     int first_distinct = 0;
     int second_distinct = 0;
 
